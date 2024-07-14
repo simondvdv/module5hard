f3c42bbe62b824630e3d0d55f9cb607507a4db55
simondv4 <simondv4@gmail.com>
2024-07-14 14:53:36 +0300

    second_finall commit

--- main.py
+++ main.py
@@ -3,7 +3,6 @@ import time
 
 
 class User:
-    user_list = []
 
     def __init__(self, name, password, age=None):
         self.name = name
@@ -13,37 +12,39 @@ class User:
     def __str__(self):
         return self.name
 
+
 class Video:
     video_names_list = []
-    video_list = []
-
-    def __init__(self, title, duration, time_now=0, adult_mode=False):
+    time_now = 0
+    def __init__(self, title, duration, adult_mode=False):
         self.title = title
         self.duration = duration
-        self.time_now = time_now
         self.adult_mode = adult_mode
 
 
 class UrTube:
-    def __init__(self, users, videos, current_user=None):
-        self.users = users
-        self.videos = videos
+    user_list = []
+    video_list = []
+
+    def __init__(self,current_user=None):
         self.current_user = current_user
 
     def log_in(self, nickname, password):
-        if nickname not in self.users.keys():
-            print('Такого пользователя нет')
-            return
-        if self.users[nickname] == hash(password):
-            self.current_user = nickname
+        for i in UrTube.user_list:
+            if nickname == i.name and password == hash(i.password):
+                self.current_user = nickname
+            elif nickname == i.name and password != hash(i.password):
+                print('Неверный пароль')
+            else:
+                print('Такого пользователя нет')
 
     def register(self, name, password, age=None):
-        for i in User.user_list:
+        for i in UrTube.user_list:
             if name == i.name:
                 print(f'Пользователь {name} уже существует')
                 return
         self.current_user = User(name, password, age)
-        User.user_list.append(User(name, password, age))
+        UrTube.user_list.append(User(name, password, age))
 
     def log_out(self):
         self.current_user = None
@@ -51,13 +52,13 @@ class UrTube:
     def add(self, *video_list):
         for i in video_list:
             if i.title not in Video.video_names_list:
-                Video.video_list.append(i)
+                UrTube.video_list.append(i)
                 Video.video_names_list.append(i.title)
 
     def get_videos(self, check_word):
         check_word = check_word.lower()
         founded_videos = []
-        for i in Video.video_list:
+        for i in UrTube.video_list:
             if check_word in i.title.lower():
                 founded_videos.append(i.title)
         if len(founded_videos) == 0:
@@ -68,7 +69,7 @@ class UrTube:
         if self.current_user is None:
             print('Войдите в аккаунт, чтобы смотреть видео')
             return
-        for i in Video.video_list:
+        for i in UrTube.video_list:
             if videos_name == i.title:
                 if i.adult_mode is True and self.current_user.age < 18:
                     print('Вам нет 18 лет, пожалуйста покиньте страницу')
@@ -88,7 +89,7 @@ class UrTube:
         return
 
 
-ur = UrTube(User.user_list, Video.video_list)
+ur = UrTube()
 v1 = Video('Лучший язык программирования 2024 года', 200)
 v2 = Video('Для чего девушкам парень программист?', 10, adult_mode=True)
 
